f8d5e286e21344771289f037176f7bbc8182fe10
Deborah <you@example.com>
2025-02-10 10:35:50 +0100

    image disparue

--- JobSearch/JobSearch/settings.py
+++ JobSearch/JobSearch/settings.py
@@ -122,9 +122,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 MEDIA_URL = "/media/"
-MEDIA_ROOT = BASE_DIR/"media"
-
-# Default primary key field type
+MEDIA_ROOT =BASE_DIR /'média'# Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
